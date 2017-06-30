9395ab440076554811ee1e25a05dc4585ca2c3cf
Max Kwon <maxkwonusa@yahoo.com>
2017-06-29 17:09:00 -0700

    selection of desired graphs added

--- PTC.py
+++ PTC.py
@@ -14,74 +14,92 @@ from tkinter.filedialog import askopenfilename
 from scipy import fftpack
 import pylab as py
 
-file = askopenfilename(filetypes=[('.fit','.fits', '.fts')],title='Pick bias 1')
-image_data = fits.getdata(file)
-header = fits.getheader(file)
-
-print('Number of Frames', len(image_data)) # shows the number of frames in the data cube (1, 2 or many )
-print('Type: ', type(image_data))
-print('Shape', image_data.shape) #dimensions of array
-print(repr(header))
-
-plt.subplot(1, 6, 1*1)
-plt.title("Initial Image")
-plt.imshow(image_data[0], origin='lower') #showing the first frame of the cube and shifting the origin to the bottom left side of the plot 
- 
-          
-med = np.median(image_data, axis=0) # will take the median of the entire cube with respect to the depth axis and display it as a frame (median frame)
-
-plt.subplot(1, 6, 1*2)
-plt.title("Median of All")
-plt.imshow(med, origin='lower')
-
+fileIn = askopenfilename(filetypes=[('.fit','.fits', '.fts')],title='Pick bias 1')
+image_data = fits.getdata(fileIn)
 
 num_frames = len(image_data) #find the length of the first dimension of the 3D array which is the number of frames
 x_dim = len(image_data[0]) #set the dimension of the x axis, returns the length of that portion of the array 
-y_dim = len(image_data[0][0]) #set the dimension of the y axis, returns the length of that portion of the array             
+y_dim = len(image_data[0][0]) #set the dimension of the y axis, returns the length of that portion of the array     
+num_pixels = x_dim * y_dim #number of total pixels in the image (height * width)  
+
+#choose which data you want (0 = no print, 1 = print)
+selection_array = [1,1,0,0,1,1,1,1] #info, initial, median, diff, SD pixel 2D, Sd pixel 1D, print median, SD per frame
+
+subplot_index = 1; # the position of a graph in the subplot (1 = left)
+
+def printInfo(file, data):
+    header = fits.getheader(file)
+    
+    print('Number of Frames', len(data)) # shows the number of frames in the data cube (1, 2 or many )
+    print('Type: ', type(data))
+    print('Shape', data.shape) #dimensions of array
+    print(repr(header))
+
+def plotInitialImage(data, subplot_pos):
+    plt.subplot(1, 6, 1*subplot_pos)
+    plt.title("Initial Image")
+    plt.imshow(data[0], origin='lower') #showing the first frame of the cube and shifting the origin to the bottom left side of the plot 
+ 
+   
+def plotMedian(data, subplot_pos):       
+    med = np.median(data, axis=0) # will take the median of the entire cube with respect to the depth axis and display it as a frame (median frame)
+    
+    plt.subplot(1, 6, 1*subplot_pos)
+    plt.title("Median of All")
+    plt.imshow(med, origin='lower')
+    
+    
+def plotDiffImage(data, subplot_pos):
+    diff_image = data[num_frames-1]-data[0] #takes the difference between the last and the first images in the 3D Array
+    
+    plt.subplot(1, 6, 1*subplot_pos)
+    plt.title("Difference")
+    plt.imshow(diff_image, origin='lower')
 
 
-diff_image = image_data[num_frames-1]-image_data[0] #takes the difference between the last and the first images in the 3D Array
+def plotSDPerPixel2D(data, subplot_pos):
+    std_devs = np.zeros(shape=(y_dim, x_dim))
+    stack = np.zeros(shape=(num_frames))
+    
+    #this for loop is really slow and somehow needs to become more efficient 
+    for i in range(0, y_dim):
+        for j in range(0, x_dim):
+           for k in range(0, num_frames):
+                stack[k] = data[k][i][j] #makes a 1D array of the depth of the cube at a certain point on the 2D array 
+                if k==(num_frames-1):
+                   std_devs[i][j] = np.std(stack) #after each 1D array is filled, the SD is taken (once at each pixel going through each frame)
+    
+    plt.subplot(1, 6, 1*subplot_pos)
+    plt.title("Standard Dev")
+    plt.imshow(std_devs, origin='lower')
+    
+    return std_devs
 
-plt.subplot(1, 6, 1*3)
-plt.title("Difference")
-plt.imshow(diff_image, origin='lower')
 
-std_devs = np.zeros(shape=(y_dim, x_dim))
-stack = np.zeros(shape=(num_frames))
-#this for loop is really slow and somehow needs to become more efficient 
-for i in range(0, y_dim):
-    for j in range(0, x_dim):
-       for k in range(0, num_frames):
-            stack[k] = image_data[k][i][j] #makes a 1D array of the depth of the cube at a certain point on the 2D array 
-            if k==(num_frames-1):
-               std_devs[i][j] = np.std(stack) #after each 1D array is filled, the SD is taken (once at each pixel going through each frame)
+def plotSDPerPixel1D(std_arr, subplot_pos):
+    sd_pixel = np.zeros(shape=(num_pixels))
+    pixel_index = range(num_pixels) #Ascending array from 0 to number of pixels - 1
+    
+    index = 0 #keeps track of the pixel number in the for loop
+    
+    #puts the pixel SD data into a 1d array in order to show it as a line graph where the x axis is the pixel number starting from the top left to tthe bottom right
+    for i in range(0, len(std_arr)): #goes through each row
+        for j in range(0, len(std_arr[0])):#goes through each column
+            sd_pixel[index] = std_arr[i][j]
+            index = index + 1
+            
+    plt.subplot(1, 6, 1*subplot_pos)
+    plt.title("SD Per Pixel")
+    plt.plot(pixel_index, sd_pixel) 
 
-plt.subplot(1, 6, 1*4)
-plt.title("Standard Dev")
-plt.imshow(std_devs, origin='lower')
 
-num_pixels = x_dim * y_dim
-sd_pixel = np.zeros(shape=(num_pixels))
-pixel_index = range(num_pixels) #Ascending array from 0 to number of pixels - 1
-
-index = 0 #keeps track of the pixel number in the for loop
-#puts the pixel SD data into a 1d array in order to show it as a line graph where the x axis is the pixel number starting from the top left to tthe bottom right
-for i in range(0, len(std_devs)): #goes through each row
-    for j in range(0, len(std_devs[0])):#goes through each column
-        sd_pixel[index] = std_devs[i][j]
-        index = index + 1
-        
-plt.subplot(1, 6, 1*5)
-plt.title("SD Per Pixel")
-plt.plot(pixel_index, sd_pixel) 
-
-print('Median SD: ', np.median(std_devs)) #the median value of the 2D array full of the SDs for each pixel 
+def printMedianSD(std_arr):
+    print('Median SD: ', np.median(std_arr)) #the median value of the 2D array full of the SDs for each pixel 
      
 
-def plotSDTime(data): #will plot the median standard deviation over time to see if the noise in the CCD increases with time and exposures done 
+def plotSDTime(data, subplot_pos): #will plot the median standard deviation over time to see if the noise in the CCD increases with time and exposures done 
     
-    mean_data = np.mean(data)          
-              
+    mean_data = np.mean(data)
     med_std_dev_frame = np.zeros(shape = (num_frames))
         
     for i in range(0, num_frames):
@@ -91,12 +109,39 @@ def plotSDTime(data): #will plot the median standard deviation over time to see 
                        
     print(med_std_dev_frame)
                        
-    plt.subplot(1, 6, 1*6)
+    plt.subplot(1, 6, 1*subplot_pos)
     plt.title("SD Over Time")
-    plt.plot(frame_index, med_std_dev_frame) 
+    plt.plot(frame_index, med_std_dev_frame)   
     
-if (num_frames > 2): #only want SD over time if it is a data cube with more than 2 frames
-    plotSDTime(image_data) 
+
+if (selection_array[0] == 1):    
+    printInfo(fileIn, image_data)   
+    
+if (selection_array[1] == 1):    
+    plotInitialImage(image_data, subplot_index)    
+    subplot_index = subplot_index + 1
+
+if (selection_array[2] == 1):
+    plotMedian(image_data, subplot_index)
+    subplot_index = subplot_index + 1
+    
+if (selection_array[3] == 1):
+    plotDiffImage(image_data, subplot_index)
+    subplot_index = subplot_index + 1
+
+if (selection_array[4] == 1):
+    sd_plot = plotSDPerPixel2D(image_data, subplot_index)
+    subplot_index = subplot_index + 1
+
+if (selection_array[5] == 1 and selection_array[4] == 1):
+    plotSDPerPixel1D(sd_plot, subplot_index)
+    subplot_index = subplot_index + 1
+
+if (selection_array[6] == 1 and selection_array[4] == 1):
+    printMedianSD(sd_plot)
+
+if (num_frames > 2 and selection_array[7] == 1): #only want SD over time if it is a data cube with more than 2 frames
+    plotSDTime(image_data, subplot_index)
     
 
 
