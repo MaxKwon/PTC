4524a0b56c6700ff86366d56dd183521a9d0cde2
Max Kwon <maxkwonusa@yahoo.com>
2017-07-03 09:43:50 -0700

    PLC function added with no way of inputting multiple files yet

--- PTC.py
+++ PTC.py
@@ -14,32 +14,41 @@ from tkinter.filedialog import askopenfilename
 from scipy import fftpack
 import pylab as py
 
-fileIn = askopenfilename(filetypes=[('.fit','.fits', '.fts')],title='Pick bias 1')
-image_data = fits.getdata(fileIn)
+#choose which data you want (0 = no print, 1 = print)
+selection_array = [1,1,1,1,1,1,1,1,1] #info, initial, median, diff, SD pixel 2D, Sd pixel 1D, print median, SD per frame, inputting multiple fits files  
+
+#TODO: figure out how to have the program input many files at once (in an array)
+num_files = 1 # 10 is temporary and for testing, not actual, not in use                   
+                  
+files = ["" for x in range(num_files)] # not in use
+image_datas = np.zeros(shape=(num_files)) # not in use
+                      
+filesIn = askopenfilename(filetypes=[('.fit','.fits', '.fts')],title='Pick bias')
+image_data = fits.getdata(fileIn) 
 
 num_frames = len(image_data) #find the length of the first dimension of the 3D array which is the number of frames
 x_dim = len(image_data[0]) #set the dimension of the x axis, returns the length of that portion of the array 
 y_dim = len(image_data[0][0]) #set the dimension of the y axis, returns the length of that portion of the array     
-num_pixels = x_dim * y_dim #number of total pixels in the image (height * width)  
-
-#choose which data you want (0 = no print, 1 = print)
-selection_array = [1,1,1,1,1,1,1,1,1] #info, initial, median, diff, SD pixel 2D, Sd pixel 1D, print median, SD per frame, inputting multiple fits files 
-
+num_pixels = x_dim * y_dim #number of total pixels in the image (height * width)                  
+                  
 subplot_index = 1; # the position of a graph in the subplot (1 = left), incremented everytime a plot is plotted in the subplot 
 
 def printInfo(file, data):
+    
     header = fits.getheader(file)
     
     print('Number of Frames', len(data)) # shows the number of frames in the data cube (1, 2 or many )
     print('Type: ', type(data))
     print('Shape', data.shape) #dimensions of array
     print(repr(header))
+    print("Process Complete printInfo")
 
 def plotInitialImage(data, subplot_pos):
+    
     plt.subplot(1, 6, 1*subplot_pos)
     plt.title("Initial Image")
     plt.imshow(data[0], origin='lower') #showing the first frame of the cube and shifting the origin to the bottom left side of the plot 
- 
+    print("Process Complete plotInitialImage")
    
 def plotMedian(data, subplot_pos):     
     
@@ -49,6 +58,8 @@ def plotMedian(data, subplot_pos):
     plt.title("Median of All")
     plt.imshow(med, origin='lower')
     
+    print("Process Complete plotMedian")
+    
     
 def plotDiffImage(data, subplot_pos):
     
@@ -57,6 +68,8 @@ def plotDiffImage(data, subplot_pos):
     plt.subplot(1, 6, 1*subplot_pos)
     plt.title("Difference")
     plt.imshow(diff_image, origin='lower')
+    
+    print("Process Complete plotDiffImage")
 
 
 def plotSDPerPixel2D(data, subplot_pos):
@@ -68,6 +81,8 @@ def plotSDPerPixel2D(data, subplot_pos):
     plt.subplot(1, 6, 1*subplot_pos)
     plt.title("Standard Dev")
     plt.imshow(std_devs, origin='lower')
+    
+    print("Process Complete plotSDPerPixel2D")
     
     return std_devs
 
@@ -82,10 +97,13 @@ def plotSDPerPixel1D(std_arr, subplot_pos):
     plt.subplot(1, 6, 1*subplot_pos)
     plt.title("SD Per Pixel")
     plt.plot(pixel_index, sd_pixel) 
+    
+    print("Process Complete plotSDPerPixel1D")
 
 
 def printMedianSD(std_arr):
-    print('Median SD: ', np.median(std_arr)) #the median value of the 2D array full of the SDs for each pixel 
+    print('Median SD: ', np.median(std_arr)) #the median value of the 2D array full of the SDs for each pixel
+    print("Process Complete printMedianSD")
      
 #TODO: Change for loop into vectorized calculation
 def plotSDTime(data, subplot_pos): #will plot the median standard deviation over time to see if the noise in the CCD increases with time and exposures done 
@@ -102,22 +120,26 @@ def plotSDTime(data, subplot_pos): #will plot the median standard deviation over
     plt.title("SD Over Time")
     plt.plot(frame_index, med_std_dev_frame)   
     
-#TODO: Change for loop into vectorizec calculation
-def plotPLC(data):
+    print("Process Complete plotSDTime")
     
-    counts = np.zeros(shape=(num_pixels)) #TODO: not sure how this is going to work exactly as of yet 
-    sorted_counts = np.zeros(shape=(num_pixels))
-                     
-    for i in range(0, y_dim):
-       for j in range(0, x_dim):
-          for k in range(0, num_frames):
-               
-              counts[i] = data[k][i][j]
-               
-    sorted_counts = sorted(counts, key=int)
-
-    print(sorted_counts)           
-               
+#TODO: Change for loop into vectorizec calculation
+#Take many data cubes and plot the median counts of each cube by the median SD of the cube 
+def plotPLC(files, subplot_pos):
+    
+    med_counts = np.zeros(shape=(num_files))
+    med_std_devs = np.zeros(shape=(num_files))
+    
+    for i in range (0, len(files)):
+        
+        data = fits.getdata(files[i])
+        
+        med_counts[i] = np.median(np.median(data))
+        
+        med_std_devs[i] = np.median(data.std(axis=0))
+        
+    plt.subplot(1, 7, 1*subplot_pos)
+    plt.title("PLC")
+    plt.plot(med_counts, med_std_devs)
     
 if (selection_array[0] == 1):    
     printInfo(fileIn, image_data)   
